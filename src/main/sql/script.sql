8cf900b987f08554c8db5ea29b6fd4e32cb002de
abdul <thiagoabdul10@gmail.com>
2023-01-03 22:36:43 -0300

    Esqueleto

---
+++ src/main/sql/script.sql
@@ -0,0 +1,14 @@
+create database if not exists login
+default charset utf8mb4
+default collate utf8mb4_general_ci;
+
+use login;
+
+create table usuario(
+    id_usuario int unsigned primary key auto_increment,
+    nome varchar(50) not null,
+    telefone char(11),
+    cep char(8) not null
+    email varchar(50) not null unique,
+    hash_senha char(40) not null
+) engine innodb;
