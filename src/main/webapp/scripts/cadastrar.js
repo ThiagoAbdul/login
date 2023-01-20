b596dc9a2bbde73ee81283b87b9883b2295b1be1
abdul <thiagoabdul10@gmail.com>
2023-01-19 21:31:57 -0300

    Desenvolvendo orientações de senha

--- src/main/webapp/scripts/cadastrar.js
+++ src/main/webapp/scripts/cadastrar.js
@@ -1,12 +1,43 @@
-import { emailInvalido, isSenhaFraca } from "./validar.js"
+import { regex, emailInvalido, senhaPossui, isSenhaFraca } from "./validar.js"
+
+const getElemento = (id) => document.querySelector(id)
 
 const btnCadastrar = document.querySelector("#btn-cadastrar")
 const divRequisitosSenha = document.querySelector("#requisitos-senha")
 const divStatusSenha = document.querySelector("#status-senha")
+const paragrafoStatusSenha = document.querySelector("#status-senha p")
 
 const getEmail = () => cadastroEmail.value
 const getSenha = () => cadastroSenha.value
 const getConfirmarSenha = () => confirmarSenha.value
+
+const confirmarRequisito = (paragrafoRequisito) => {
+    const classesDoParagrafo = paragrafoRequisito.classList
+    if(classesDoParagrafo.contains('check') == false){
+        classesDoParagrafo.add('check')
+        classesDoParagrafo.remove('uncheck')
+    }
+}
+
+const negarRequisito = (paragrafoRequisito) => {
+    const classesDoParagrafo = paragrafoRequisito.classList
+    if(classesDoParagrafo.contains('check')){
+        classesDoParagrafo.add('uncheck')
+        classesDoParagrafo.remove('check')
+    }
+}
+
+const marcarRequisito = (requisitoAtendido, paragrafoRequisito) => {
+    
+    if(requisitoAtendido()){
+        confirmarRequisito(paragrafoRequisito)
+    }
+    else{
+        negarRequisito(paragrafoRequisito)
+    }
+}
+
+const verificarSenhaFactory = (regex) => () => senhaPossui(getSenha(), regex)
 
 const mostrarRequisitosDaSenha = () => {
     divRequisitosSenha.style.display = "block"
@@ -20,7 +51,30 @@ const resumirStatusDaSenha = () => {
 
 cadastroSenha.addEventListener('focus', mostrarRequisitosDaSenha)
 
-cadastroSenha.addEventListener('blur', resumirStatusDaSenha)
+cadastroSenha.addEventListener('keyup', () => {
+    marcarRequisito(verificarSenhaFactory(regex['CARACTER_MINUSCULO']), 
+                        getElemento('#caracter-minusculo'))
+    marcarRequisito(verificarSenhaFactory(regex['CARACTER_MAIUSCULO']), 
+                        getElemento('#caracter-maiusculo'))
+    marcarRequisito(verificarSenhaFactory(regex['CARACTER_ESPECIAL']), 
+                        getElemento('#caracter-especial'))
+    marcarRequisito(verificarSenhaFactory(regex['CARACTER_NUMERICO']), 
+                        getElemento('#caracter-numerico'))
+    marcarRequisito(verificarSenhaFactory(regex['DOZE_CARACTERES']), 
+                        getElemento('#doze-caracteres'))                    
+})
+
+cadastroSenha.addEventListener('blur', () => {
+    if(isSenhaFraca(getSenha())){
+        paragrafoStatusSenha.innerHTML = "Senha não atende os requisítos"
+        negarRequisito(paragrafoStatusSenha)
+    }
+    else{
+        paragrafoStatusSenha.innerHTML = "Senha atende os requisítos"
+        confirmarRequisito(paragrafoStatusSenha)
+    }
+    resumirStatusDaSenha()
+})
 
 btnCadastrar.addEventListener('click', () => {
     if(emailInvalido(getEmail())){
