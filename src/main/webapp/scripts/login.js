693c9a0caad21fc2ab547a844e079d089d42a4ff
abdul <thiagoabdul10@gmail.com>
2023-01-21 16:24:27 -0300

    Verificações e praticidades

--- src/main/webapp/scripts/login.js
+++ src/main/webapp/scripts/login.js
@@ -1,19 +1,38 @@
-import { emailInvalido, isSenhaFraca } from './validar.js'
+import { emailInvalido, senhaFraca } from './validar.js'
 
 const btnEntrar = document.querySelector("#btn-entrar")
 
-const getEmail = () => document.querySelector("#login-email").value
-const getSenha = () => document.querySelector("#login-senha").value
+const getEmail = () => loginEmail.value
+const getSenha = () => loginSenha.value
 
 
-btnEntrar.addEventListener('click', () => {
+const logar = () => {
     if(emailInvalido(getEmail())){
         alert("Favor preencher seu e-mail corretamente")
+        loginEmail.focus()
     }
-    else if(isSenhaFraca(getSenha())){
+    else if(senhaFraca(getSenha())){
         alert("Senha não atende os critérios")
+        loginSenha.focus()
     }
     else {
         formLogin.submit()
     }
+}
+
+loginEmail.addEventListener('keyup', e => {
+    if(e.key === 'Enter'){
+        if(emailInvalido(getEmail())){
+            alert("Favor preencher seu e-mail corretamente")
+        }
+        else{
+            loginSenha.focus()
+        }
+    } 
 })
+
+loginSenha.addEventListener('keyup', e => {
+    if(e.key === "Enter") logar()
+})
+
+btnEntrar.addEventListener('click', logar)
